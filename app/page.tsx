3b941764c284b5b2e726c8d46eff748bef2da434
Avishake Adhikary <avhishe.adhikary11@gmail.com>
2025-01-30 19:05:39 +0530

    Added dark mode toggle support

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-
+import { ModeToggle } from "@/components/mode-toggle";
 
 
 export default function Home() {
   return (
     <div className="flex w-full h-full p-8 gap-8 justify-center items-center flex-col">
+      <ModeToggle></ModeToggle>
       <div className="font-alfphabet">Welcome to</div>
       <div className="font-lobster text-5xl">Suit-Up Lounge</div>
       <iframe src="https://discord.com/widget?id=1282080392039370854&theme=dark" width="350" height="500" allowTransparency={true} frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts" className="rounded"/>
